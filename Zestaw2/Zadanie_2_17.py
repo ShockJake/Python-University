06a69d3509137a464f0d0318b46af748f6d90fb9
Stanislav <87501613+ShockJake@users.noreply.github.com>
2021-10-26 12:48:55 +0200

    Create Zadanie_2_17.py

---
+++ Zestaw2/Zadanie_2_17.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class Sort(Enum):
+    BY_ALPHABET = 0
+    BY_LENGTH = 1
+
+def main():
+
+    str = 'Stanly Chris Jane Ray'
+
+    sort(str, Sort.BY_LENGTH)
+
+
+def sort(s: str, sort_by: Sort):
+    words = s.split(' ')
+    if sort_by == Sort.BY_ALPHABET:
+        print(sorted(words))
+    elif sort_by == Sort.BY_LENGTH:
+        print(sorted(words, key=len))
+
+
+if __name__ == "__main__":
+    main()
