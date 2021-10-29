501d16dc6401d6f08a885e22b6ad43477da9e4ee
Stanislav Lysunenko <sidux77@gmail.com>
2021-10-29 08:59:13 +0200

    Added z_4_6

---
+++ Zestaw4/Zadanie_4_6.py
@@ -0,0 +1,22 @@
+# Napisać funkcję sum_seq(sequence) obliczającą sumę liczb zawartych w sekwencji,
+# która może zawierać zagnieżdżone podsekwencje. Wskazówka: rozważyć wersję rekurencyjną,
+# a sprawdzanie, czy element jest sekwencją, wykonać przez isinstance(item, (list, tuple)).
+
+def sum_seq(sequence, result):
+    for i in sequence:
+        if isinstance(i, int):
+            result += i
+        else:
+            result = sum_seq(i, result)
+
+    return result
+
+
+def main():
+    sequence = [1, 2, 3, [1, 1], [2, [4, 7]]]
+    result = 0
+    print(sum_seq(sequence, result))
+
+
+if __name__ == '__main__':
+    main()
