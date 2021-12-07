5119c79f75ef0932fb247bbb00670d65781c38db
Stanislav Lysunenko <sidux77@gmail.com>
2021-12-07 22:44:34 +0100

    Added Z.8.1

--- Zestaw8/Zadania_8_1.py
+++ Zestaw8/Zadania_8_1.py
@@ -1,15 +1,28 @@
+# -*- coding utf-8 -*-
 # Zbadać problem szukania rozwiązań równania liniowego postaci a * x + b * y + c = 0.
 # Podać specyfikację problemu. Podać algorytm rozwiązania w postaci listy kroków,
 # schematu blokowego, drzewa. Podać implementację algorytmu w Pythonie w postaci funkcji solve1(),
 # która rozwiązania wypisuje w formie komunikatów.
 
-
 def solve1(a, b, c):
-    pass
+    if a == 0 and b != 0:
+        y = -(c / b)
+        print('Pierwiastkiem równania jest każda para liczb w postaci (r, ' +
+              str(y) + '), gdize r wybieramy dowolnie.')
+    if a != 0 and b == 0:
+        x = -(c / a)
+        print('Pierwiastkiem równania jest każda para liczb w postaci (' +
+              str(x) + ', r), gdize r wybieramy dowolnie.')
+    if a != 0 and b != 0:
+        y1 = -(a / b)
+        y2 = -(c / b)
+        y = str(y1) + ' * r - ' + str(y2)
+        print('Pierwiastkiem równania jest każda para liczb w postaci (r, ' +
+              y + '), gdize r wybieramy dowolnie.')
 
 
 def main():
-    pass
+    solve1(2, 3, 4)
 
 
 if __name__ == '__main__':
