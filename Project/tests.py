4fe2d76b6510d39cf4dc6d063379a6ab799fc22e
Stanislav Lysunenko <sidux77@gmail.com>
2022-01-19 10:33:43 +0100

    Added tests

--- Project/tests.py
+++ Project/tests.py
@@ -11,23 +11,25 @@ class TestTree(unittest.TestCase):
         self.tree.insert(9)
         self.tree.insert(8)
         self.tree.insert(10)
+        self.tree.insert(11)
+        self.tree.insert(13)
     
     def test_in_order(self):
-        self.assertEqual(self.tree.printInOrder(), '8 9 10 ')
+        self.assertEqual(self.tree.printInOrder(), '8 9 10 11 13 ')
         
     def test_pre_order(self):
-        self.assertEqual(self.tree.printPreOrder(), '9 8 10 ')
+        self.assertEqual(self.tree.printPreOrder(), '9 8 11 10 13 ')
         
     def test_post_order(self):
-        self.assertEqual(self.tree.printPostOrder(), '8 10 9 ')
+        self.assertEqual(self.tree.printPostOrder(), '8 10 13 11 9 ')
     
     def test_insert(self):
-        self.tree.insert(11)
-        self.assertEqual(self.tree.printInOrder(), '8 9 10 11 ')
+        self.tree.insert(15)
+        self.assertEqual(self.tree.printInOrder(), '8 9 10 11 13 15 ')
     
     def test_remove(self):
         self.tree.remove(8)
-        self.assertEqual(self.tree.printInOrder(), '9 10 ')
+        self.assertEqual(self.tree.printInOrder(), '9 10 11 13 ')
     
     def test_contains(self):
         self.assertTrue(self.tree.contains(10))
